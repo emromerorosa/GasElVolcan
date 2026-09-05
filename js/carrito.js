7f31cbf134a10ee769b0d8e72eee8edad7e5d2c5
Daniel Moisés Soto Ligena <daniel.soto.ligena@gmail.com>
2026-09-05 17:30:04 -0400

    Agregando funcion agregarCarrito con validacion del stock actual

--- js/carrito.js
+++ js/carrito.js
@@ -11,3 +11,36 @@ function obtenerCarrito() {
     }
     return JSON.parse(texto);
 }
+
+function agregarAlCarrito(codigo, cantidad) {
+  const producto = PRODUCTOS.find(function (p) {
+    return p.codigo === codigo;
+  });
+
+  if (producto === undefined) {
+    console.error("Producto no encontrado:", codigo);
+    return;
+  }
+
+  const carrito = obtenerCarrito();
+
+  const itemExistente = carrito.find(function (item) {
+    return item.codigo === codigo;
+  });
+
+  const cantidadActual = itemExistente ? itemExistente.cantidad : 0;
+  const cantidadNueva = cantidadActual + cantidad;
+
+  if (cantidadNueva > producto.stock) {
+    alert("No hay suficiente stock. Disponible: " + producto.stock);
+    return;
+  }
+
+  if (itemExistente) {
+    itemExistente.cantidad = cantidadNueva;
+  } else {
+    carrito.push({ codigo: codigo, cantidad: cantidadNueva });
+  }
+
+  guardarCarrito(carrito);
+}
