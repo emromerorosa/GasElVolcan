bdb32ef6c68124e39671dc71a3104600f94e5198
Rosa Meza <emromerorosa@gmail.com>
2026-09-06 01:58:43 -0300

    fix admin productos con id

--- js/admin-productos.js
+++ js/admin-productos.js
@@ -1,50 +1,24 @@
-var productosBase = [
-    {id: 1, nombre: "Gas 5kg", precio: 12000, stock: 30},
-    {id: 2, nombre: "Gas 15kg", precio: 25000, stock: 50},
-    {id: 3, nombre: "Gas 45kg", precio: 65000, stock: 20}
-];
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Admin - Productos</title>
+<link rel="stylesheet" href="../css/estilos.css">
+</head>
+<body>
+<h1>Admin - Productos</h1>
+<a href="home.html">Inicio Admin</a> |
+<a href="producto-nuevo.html">Agregar Producto</a> |
+<a href="usuarios.html">Usuarios</a>
+<br><br>
+<table border="1" cellpadding="10">
+<thead>
+<tr><th>ID</th><th>Nombre</th><th>Precio</th><th>Stock</th><th>Acciones</th></tr>
+</thead>
+<tbody id="tabla-productos"></tbody>
+</table>
 
-var productos = productosBase;
-
-try {
-    var guardado = localStorage.getItem("productos");
-    if(guardado){
-        var temp = JSON.parse(guardado);
-        if(temp && temp.length > 0){
-            // Si el guardado viene sin ID, le ponemos ID automaticamente
-            for(var j=0; j<temp.length; j++){
-                if(!temp[j].id){
-                    temp[j].id = j+1;
-                }
-            }
-            productos = temp;
-        }
-    }
-} catch(e){
-    productos = productosBase;
-}
-
-var tabla = document.getElementById("tabla-productos");
-tabla.innerHTML = "";
-
-for(var i=0; i<productos.length; i++){
-    var id = productos[i].id? productos[i].id : (i+1);
-    var nombre = productos[i].nombre? productos[i].nombre : "";
-    var precio = productos[i].precio? productos[i].precio : 0;
-    var stock = productos[i].stock? productos[i].stock : 0;
-
-    tabla.innerHTML += "<tr><td>"+id+"</td><td>"+nombre+"</td><td>"+precio+"</td><td>"+stock+"</td><td><button onclick='editar("+i+")'>Editar</button> <button onclick='eliminar("+i+")'>Eliminar</button></td></tr>";
-}
-
-function editar(i){
-    localStorage.setItem("indiceEditar", i);
-    window.location.href = "producto-editar.html";
-}
-
-function eliminar(i){
-    if(confirm("¿Eliminar este producto?")){
-        productos.splice(i,1);
-        localStorage.setItem("productos", JSON.stringify(productos));
-        location.reload();
-    }
-}
+<script src="../js/admin-productos.js"></script>
+</body>
+</html>
